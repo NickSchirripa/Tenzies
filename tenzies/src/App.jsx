f611864cf764bd8cd2d2e2c43b14d17de808818f
Nick Schirripa <108542730+NickSchirripa@users.noreply.github.com>
2025-07-09 15:35:27 -0400

    setup
    -basic setup for a tenzie game

---
+++ tenzies/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './index.css'
+import Die from "./Die.jsx"
+
+export default function App() {
+  return(
+    <main>
+    <div className='gridContainer'>
+    <Die
+    value="3"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    <Die
+    value="2"
+    />
+    </div>
+    </main>
+  )
+}
+
+
---
+++ tenzies/src/Die.jsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+export default function Die(props){
+return(
+<button className='die'>{props.value}</button>
+)
+}
