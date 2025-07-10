3309719c39f0e4bb7b729d858e13147a77a8bd96
Nick Schirripa <108542730+NickSchirripa@users.noreply.github.com>
2025-07-10 15:22:28 -0400

    update
    -made a button that has a callback function that changes state and rolls dice
    -changed function generateAllNewDice() return an array of objects

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -4,23 +4,49 @@ import Die from "./Die.jsx"
 
 export default function App() {
 
+  //generates 10 random numbers and pushes them to a new array and returns the values.
+  
+//function generateAllNewDice(){
+ //return new Array(10)
+ // .fill(0)
+  //.map(()=>Math.floor(Math.random()*6)+1)
+
+//change the function above, so it returns an object with properties. One property being a value, with our random number, and another a boolean
 function generateAllNewDice(){
- return new Array(10)
+  return new Array(10)
   .fill(0)
-  .map(()=>Math.floor(Math.random()*6)+1)
+  .map(()=>({
+    value:Math.ceil(Math.random() * 6),
+    isHeld:false
+  }))
 }
 
-const [numbers, setNumbers] = useState(generateAllNewDice())
-const diceElement = numbers.map(numberDie=> <Die value={numberDie}/>)
 
+//assigns the random number array to a useState variable
+const [numbers, setNumbers] = useState(generateAllNewDice())
+
+//maps over the array of random numbers and renders the Die Comp, and gives a random number in the array to each Die comp Value Prop
+const diceElement = numbers.map(numberDie=> <Die value={numberDie.value}/>)
+
+
+
+//a function that generates new numbers for "numbers" array. 
+// So when this is used it will change state, and thus render new numebrs for the diceElement.
+//we will use this as a callback function for then button below, which when clicked will trigger the function
+function rollDice(){
+  setNumbers(generateAllNewDice())
+}
 
   return(
     <main>
     <div className='gridContainer'>
-    {diceElement}
+    {diceElement}  
+    
     </div>
+<button className='rollBtn' onClick={rollDice}>Roll</button>
     </main>
   )
 }
+
 
 
